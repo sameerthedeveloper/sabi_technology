ca30fee28194236980a868912b7c031c1ecc0a67
Mohamed Sameer S <mohamedsameer.s.2007@gmail.com>
2025-06-27 21:19:18 +0530

    Refactor index.html layout and enhance tab control functionality in tab_controll.js

--- js/tab_controll.js
+++ js/tab_controll.js
@@ -1,6 +1,7 @@
 const items = document.querySelectorAll('.item');
 const home = document.querySelector('.home');
 const invoice = document.querySelector('.quta');
+const info = document.querySelector('.info');
 
 
 // invoice.style.display = 'none';
@@ -20,9 +21,16 @@ items.forEach(item => {
         if (item.classList.contains('home')) {
             home.style.display = 'block';
             invoice.style.display = 'none';
+            info.style.display = 'none';
         } else if (item.classList.contains('invoice')) {
             home.style.display = 'none';
             invoice.style.display = 'block';
+            info.style.display = 'none';
+        }
+        else if (item.classList.contains('info')) {
+            home.style.display = 'none';
+            invoice.style.display = 'block';
+            info.style.display = 'block';
         }
     });
 });
