d09da566ea7426a69553654564ef7a5c81e27a75
Mohamed Sameer S <mohamedsameer.s.2007@gmail.com>
2025-06-27 23:47:34 +0530

    Add IDs to input elements in index.html and initialize them in app.js

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const scrPixel = document.querySelector('#scrpixel');
+const csWidth = document.querySelector('#cswidth');
+const csHeight = document.querySelector('#csheight');
+const modHeight = document.querySelector('#modheight');
+const modWidth = document.querySelector('#modwidth');
+
+const modmat_w = document.querySelector('#modmat_w');
+const modmat_h = document.querySelector('#modmat_h');
+
+const scrSize_mm_w = document.querySelector('#scrsize_mm_w');
+const scrSize_mm_h = document.querySelector('#scrsize_mm_h');
+
+const scrSize_ft_w = document.querySelector('#scrsize_ft_w');
+const scrSize_ft_h = document.querySelector('#scrsize_ft_h');
+
+
+const totSqft = document.querySelector('#totSqft');
+
+const costPerSqft = document.querySelector('#costpersqft');
+
+
+const modQty = document.querySelector('#modqty');
+const modSpare = document.querySelector("#modspare");
+const modUPrice = document.querySelector('#moduprice');
+const modTotAmt = document.querySelector('#modtotamt');
+
+const smpsQty = document.querySelector('#smpsqty');
+const smpsSpare = document.querySelector('#smpsspare');
+const smpsUPrice = document.querySelector('#smpsuprice');
+const smpsTotAmt = document.querySelector('#smpstotamt');
+
+const rxQty = document.querySelector('#rxqty');
+const rxUPrice = document.querySelector('#rxuprice');
+const rxTotAmt = document.querySelector('#rxtotamt');
+
+const txQty = document.querySelector('#txqty');
+const txUPrice = document.querySelector('#txuprice');
+const txTotAmt = document.querySelector('#txtotamt');
+
+const cableQty = document.querySelector('#cableqty');
+const cableUPrice = document.querySelector('#cableuprice');
+const cableTotAmt = document.querySelector('#cabletotamt');
+
+const totAmt = document.querySelector('#totamt');
